9de0080db7eb5245caddccbee2ccd1c5781e75a9
eduard023 <edo.avdzh@yandex.ru>
2023-02-08 17:27:50 +0300

    Initial commit

---
+++ src/main/java/com/example/homeworkmockito/HomeWorkMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkMockitoApplication.class, args);
+	}
+
+}
