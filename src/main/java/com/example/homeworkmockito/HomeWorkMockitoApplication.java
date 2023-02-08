d477904d99c014f4df0f4af48ec12f004d151361
eduard023 <edo.avdzh@yandex.ru>
2023-02-08 17:28:23 +0300

    первый коммит

--- src/main/java/com/example/homeworkmockito/HomeWorkMockitoApplication.java
+++ src/main/java/com/example/homeworkmockito/HomeWorkMockitoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeWorkMockitoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeWorkMockitoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkMockitoApplication.class, args);
+    }
 
 }
